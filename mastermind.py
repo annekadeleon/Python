fbe7342ec3acbca5b42232fb4109769014eafd87
Anneka de Leon <annsophdl@gmail.com>
2018-12-30 22:08:43 +0800

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -7,36 +7,46 @@ num2 = random.randint(0, 9)
 num3 = random.randint(0, 9)
 num4 = random.randint(0, 9)
 
-nums = [num1, num2, num3, num4]
+nums = str(num1) + str(num2) + str(num3) + str(num4)
+stars = ""
+guess = ""
+
 print nums
 
-print("Guess the 4 numbers in as few tries as possible")
+print("Guess the 4 numbers in as few tries possible")
 
-count = 0
-out = ""
+guess = raw_input(">")
 
-guess = raw_input()
-guess = [guess]
+while stars != "****":
+	for num in guess:
+		if num == guess[0]:
+			stars = stars + "*"
+		elif num == guess[1]:
+			stars = stars + "*"
+		elif num == guess[2]:
+			stars = stars + "*"
+		elif num == guess[3]:
+			stars = stars + "*"
+		else:
+			print("wrong")
+	print stars
 
-if ((nums == guess).all()):
-	print("****")
-#if (guess[0][0] in nums):
-#	out = out + "*"
-#elif (guess[0][1] in nums):
-#	out = out + "*"
-#elif (guess[0][2] in nums):
-#	out = out + "*"
-#elif (guess[0][3] in nums):
-#	out = out + "*"
-#else:
-#	print "none"
-#print out
 
-#if ((guess[0][0] in nums) and (guess[0][1] in nums) and (guess[0][2] in nums) and (guess[0][3] in nums)):
-#	print("****")
-#else:
-#	print("none")
-
-#for char in guess:
-#	if ((nums[0][0] in guess) and (nums[0][1] in guess) and (nums[0][2] in guess) and (nums[0][3] in guess)):
-#		print("****")
+#while stars != "****":
+#	guess = raw_input(">")
+#	if (nums[0]in guess)
+#	elif nums[0] in guess:
+#		stars = stars + "*"
+#		print "stars: " + stars
+#	elif nums[1] in guess:
+#		stars = stars + "*"
+#		print "stars: " + stars
+#		print stars
+#	elif nums[2] in guess:
+#		stars = stars + "*"
+#		print "stars: " + stars
+#	elif nums[3] in guess:
+#		stars = stars + "*"
+#		print "stars: " + stars
+#	else:
+#		print "try again"#
