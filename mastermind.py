bdad06c320a4605c53eb37361ea4328eddc5325d
Anneka de Leon <annsophdl@gmail.com>
2018-12-31 16:50:01 +0800

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -8,27 +8,44 @@ num3 = random.randint(0, 9)
 num4 = random.randint(0, 9)
 
 nums = [num1, num2, num3, num4]
-print nums
-
-print("Guess the 4 numbers in as few tries as possible")
-
+tries = 0
 count = 0
 
-while count <= 4:
-	guess = raw_input()
-	for char in guess:
-		if int(char) == nums[count]:
-			print("*")
-	count += 1
+#print nums
 
-#if difficulty.lower() = "a" or "easy":
-#	print("Guess the number in as few tries as possible")
-#	num = randint(0, 9)
-#	guess = raw_input()
-#	if int(guess) == num:
-#		score += 1
-#		print("Well done!")
-#	elif int(guess) != num:
-#		print("Sorry!")
-#	else:
-#		print("Wrong input!")
+print("Guess the 4 numbers in as few tries possible")
+
+while True:
+	stars = ""
+	remaining_nums = nums[:]
+	guess = raw_input(">")
+	tries += 1
+	if len(guess) != 4:
+		print("You must guess 4 digits. Try again.")
+	else:
+		guess = [int(guess[0]), int(guess[1]), int(guess[2]), int(guess[3])]
+		if nums == guess:
+			print("Match!")
+			break
+
+		if (guess[0] in nums) or (guess[1] in nums) or (guess[2] in nums) or (guess[3] in nums):
+			if guess[0] in remaining_nums:
+				stars += "*"
+				remaining_nums.remove(guess[0])
+			if guess[1] in remaining_nums:
+				stars += "*"
+				remaining_nums.remove(guess[1])
+			if guess[2] in remaining_nums:
+				stars += "*"
+				remaining_nums.remove(guess[2])
+			if guess[3] in remaining_nums:
+				stars += "*"
+				remaining_nums.remove(guess[3])
+			print stars
+
+		else:
+			print("Wrong guess. Try again.")
+
+	if stars == "****":
+		print("You guessed all the numbers in " + str(tries) + " tries!")
+		break
